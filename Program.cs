9fd93d199fd04e9efae57363249c1fde71e05524
RaccoonJ <j0542351@gmail.com>
2023-01-09 13:45:07 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Josh_Slaunwhite_VSTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
